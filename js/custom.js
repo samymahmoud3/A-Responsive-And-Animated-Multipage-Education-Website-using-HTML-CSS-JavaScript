09d8894fc28d072aaa03a003fab5f8c737551311
S A M Y <samymahmoud2532001@gmail.com>
2023-02-23 19:02:24 +0200

    Javascript code to show answers of questions

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,16 @@
 window.addEventListener('scroll', () => {
     document.querySelector('.nav').classList.toggle
         ('window-scroll', window.scrollY > 0)
-})
+});
+
+//show the answers of questions
+
+const faqs = document.querySelectorAll('.FAQs_card');
+
+faqs.forEach(myFunction);
+
+function myFunction(element) {
+    element.addEventListener('click', () => {
+        element.classList.toggle('open');
+    });
+}
