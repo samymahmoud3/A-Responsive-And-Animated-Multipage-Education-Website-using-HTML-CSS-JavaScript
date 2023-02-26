422b90bcaeb85148c5c2be7e99713989ddb37007
S A M Y <samymahmoud2532001@gmail.com>
2023-02-26 13:05:26 +0200

    nav-bar for tablets

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,32 @@ function myFunction(element) {
         element.classList.toggle('open');
     });
 }
+
+//side navigation bar
+var menu = document.querySelector('.fa-bars');
+var sidebar = document.querySelector('.nav-links');
+
+menu.addEventListener('click', () => {
+    if (menu.className === "fa fa-bars") {
+        sidebar.style.display = "block";
+    }
+    else {
+        sidebar.style.display = "none";
+    };
+
+    if (menu.className === "fa fa-bars") {
+        menu.className = "fa fa-times"
+    }
+    else {
+        menu.className = "fa fa-bars"
+    };
+
+});
+window.onresize = function () {
+    if (window.innerWidth > 1024) {
+        sidebar.style.display = "block";
+    } else {
+        sidebar.style.display = "none";
+        menu.className = "fa fa-bars"
+    }
+}
